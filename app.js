6a183b36e014b6bb0c3d1a03cd8dc70435f8077e
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-04-28 21:44:59 -0300

    add get /participants

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
-import express, {json} from "express";
-import chalk from "chalk";
+import express from 'express';
+import cors from 'cors';
+import chalk from 'chalk';
+import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(json())
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+app.post('/participants', async (req, res) => {
+    try {
+        const {name} = req.body;
+    
+		if (!name) {
+			await mongoClient.connect();
+			const db = mongoClient.db("BatepapoUol");
+
+			db.collection("participants").insertOne({
+				name: req.body.name, 
+				lastStatus: Date.now()
+			});
+			res.statusCode(201);
+			mongoClient.close();
+		}
+				
+	 } catch (error) {
+	  res.status(500).send('A culpa foi do estagiário')
+	
+	 }
+
+})
+
+app.get('/participants', async (req, res) => {
+
+    try {
+
+		await mongoClient.connect();
+		const db = mongoClient.db("BatepapoUol");
+        
+        const participants = await db.collection("participants").find().toArray()
+				
+		res.send(participants);
+		mongoClient.close();
+	 } catch (error) {
+	  res.status(500).send('A culpa foi do estagiário').send(error)
+		mongoClient.close()
+	 }
+
+})
 
 
-app.listen(3000, () => {
-    console.log(chalk.bold.green("Server is running on port 3000"));
+
+app.listen(5000, () => {
+    console.log(chalk.bold.green("Server is running on port 5000"));
 });
